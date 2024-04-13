1c16ac572acbc03de34ae3ad2c0f2ca96b421ea7
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-04-13 19:45:05 +0530

    node js

--- 1-http-logger/index.js
+++ 1-http-logger/index.js
@@ -1,8 +1,15 @@
 const http = require("http");
+const fs = require("fs");
 const port = 8080;
 
 const server = http.createServer((req,res)=>{
-    console.log(`Hello, The Server Got hit by ${req.socket.remoteAddress}`);
+    const logMessage = `Hello, The Server Got hit by ${req.socket.remoteAddress}\n`;
+    console.log(logMessage);
+    fs.appendFile("logs.txt", logMessage, (err) => {
+        if (err) {
+            console.error("Error writing to log file:", err);
+        }
+    });
     res.end("Hello World From the Server : 8080");
 });
 
