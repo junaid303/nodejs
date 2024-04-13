6287b563d818b9785d005050cf9b28e2db8bc0a4
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-04-13 20:29:25 +0530

    node js

--- 1-http-logger/index.js
+++ 1-http-logger/index.js
@@ -2,17 +2,29 @@ const http = require("http");
 const fs = require("fs");
 const port = 8080;
 
-const server = http.createServer((req,res)=>{
-    const logMessage = `Hello, The Server Got hit by ${req.socket.remoteAddress}\n`;
+const server = http.createServer((req, res) => {
+    // Capture request details
+    const remoteAddress = req.socket.remoteAddress;
+    const userAgent = req.headers['user-agent'];
+    const referer = req.headers['referer'] || 'Not specified';
+    const dateTime = new Date().toISOString();
+    const method = req.method;
+    const url = req.url;
+
+    // Log request details
+    const logMessage = `HTTP Req Log\nRemote Address: ${remoteAddress}\nUser-Agent: ${userAgent}\nDate-Time: ${dateTime}\nHTTP Method: ${method}\nURL: ${url}\n`;
     console.log(logMessage);
     fs.appendFile("logs.txt", logMessage, (err) => {
         if (err) {
             console.error("Error writing to log file:", err);
         }
     });
+
+    // Respond to the request
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end("Hello World From the Server : 8080");
 });
 
-server.listen(port, ()=>{
+server.listen(port, () => {
     console.log(`Server Started at port : ${port}`);
 });
