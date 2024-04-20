eec8bf6b78a09349a4197a585a83aa69afa4bbc4
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-04-21 03:09:49 +0530

    http logger middleware destructured code

--- 1-http-logger/index.js
+++ 1-http-logger/index.js
@@ -13,7 +13,7 @@ server.listen(port, () => {
     console.log(`Server Started at ${port}`);
 });
 
-
+/*
 function logRequestDetails(req, res) {
     const remoteAddress = req.connection.remoteAddress;
     const userAgent = req.headers['user-agent'];
@@ -33,7 +33,27 @@ function logRequestDetails(req, res) {
         .then(() => console.log("Log message appended to logs.txt"))
         .catch((err) => console.error("Error appending log message to logs.txt:", err));
 }
+*/
+function logRequestDetails(req, res) {
+    const {
+        connection: { remoteAddress },
+        headers: { 'user-agent': userAgent, referer },
+        method,
+        url,
+    } = req;
+    const { statusCode } = res;
+    const dateTime = new Date().toISOString();
+    const contentLength = res.getHeader('content-length') || '-';
 
+    const logMessage = `Remote Address: ${remoteAddress} - User-Agent: ${userAgent} - Date-time: ${dateTime} - Method: ${method} -URL: ${url} -Status Code: ${statusCode} -Referer: ${referer || '-'} - Content-length: ${contentLength}\n`;
+
+    console.log(logMessage); // Log to console
+
+    // Append log message to logs.txt file
+    fs.appendFile("logs.txt", logMessage)
+        .then(() => console.log("Log message appended to logs.txt"))
+        .catch((err) => console.error("Error appending log message to logs.txt:", err));
+}
 
 
 
