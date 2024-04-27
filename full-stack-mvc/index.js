dc0f96b842584ec2802d3865261fd11a96835a63
junaid303 <mohammedjunaidkhan7@gmail.com>
2024-04-27 12:54:24 +0530

    http-logger

--- full-stack-mvc/index.js
+++ full-stack-mvc/index.js
@@ -1,8 +1,23 @@
 import http from "http";
-import fs from "fs/promises";
-import url from "url";
+import { promises as fs } from "fs";
+// import url from "url";
 
 const port = 8080;
+
+
+const server = http.createServer((req,res)=>{
+    
+        //HTTP Middleware 
+        logRequestDetails(req,res)
+         res.end("Hello World from Server : 8080")
+            
+    
+});
+
+
+server.listen(port, ()=>{
+    console.log(`Server Started at ${port}`);
+});
 
 function logRequestDetails(req, res) {
     const remoteAddress = req.connection.remoteAddress;
@@ -23,60 +38,3 @@ function logRequestDetails(req, res) {
         .then(() => console.log("Log message appended to logs.txt"))
         .catch((err) => console.error("Error appending log message to logs.txt:", err));
 }
-
-const server = http.createServer(async (req,res)=>{
-    try {
-        //HTTP Middleware 
-        logRequestDetails(req,res)
-         //It Extracts pathname, searchparams from req.url object
-         const parsedURL = url.parse(req.url, true);
-          /*
-            API Endpoint : /api/tasks
-            HTTP Method : GET
-            Data Validations  : None 
-            Desc : Read all the tasks from data.json and send the data as response
-        */
-           if(req.method==="GET" && parsedURL.pathname==="/api/tasks"){
-            let data = await fs.readFile("data.json");
-            res.setHeader("Access-Control-Allow-Origin","*");
-            res.writeHead(200,{'Content-Type':'application/json'});
-            setTimeout(()=>{
-                res.end(data);
-            },2000);
-           }
-
-             /*
-           API Endpoint : /api/task
-           HTTP Method : GET
-           Params : _id
-           Data Validations  : None 
-           Desc : Read all the tasks from data.json and send the _id task as response
-       */
-        else if (req.method === "GET" && parsedURL.pathname === "/api/task") {
-            let _id = parsedURL.query._id;
-            let data = JSON.parse(await fs.readFile("data.json"));
-            let index = data.findIndex(ele => ele._id === _id);
-
-            if (index === -1) {
-                res.writeHead(400, { 'Content-Type': "application/json" });
-                return res.end(JSON.stringify({ message: "Invalid ID." }));
-            }
-
-            let task = data.find(ele => ele._id === _id);
-            res.writeHead(200, { 'Content-Type': "application/json" });
-            res.end(JSON.stringify(task));
-        }
-        
-    } catch (error) {
-        // console.log(error);
-        res.writeHead(500, { 'Content-Type': "application/json" });
-        res.end(JSON.stringify({ error: "Something Went Wrong. Try Again Later" }));
-    }
-
-    
-});
-
-
-server.listen(port, ()=>{
-    console.log(`Server Started at ${port}`);
-});
